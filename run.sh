1e88c81dd86196aab47bc1b38a88e7c72bd0d7dd
Jan Forberg <jan.forberg@mailbox.tu-dresden.de>
2020-02-07 12:01:41 +0100

    fixed the all-data query

--- run.sh
+++ run.sh
@@ -1,24 +1,42 @@
 FOLDER=$DATAPATH
 
 touch $DATAPATH/download.lck
+rm $DATAPATH/*.ttl
 
 rm -r /tmp/databusdump
 mkdir /tmp/databusdump
 
 # pull the services
+echo "Pulling services data"
 rapper -i turtle -o ntriples https://raw.githubusercontent.com/dbpedia/databus-content/master/services.ttl > /tmp/databusdump/services.ttl
 
 # pull the accounts data
+echo "Pulling accounts data"
 rapper -i turtle -o ntriples https://raw.githubusercontent.com/dbpedia/accounts/master/accounts.ttl > /tmp/databusdump/accounts.ttl
 
 # pull the webids
+echo "Pulling webid data"
 rapper -i turtle -o ntriples https://raw.githubusercontent.com/dbpedia/accounts/master/accounts.ttl | cut -f1 -d " " | sed 's/<//;s/>//' > /tmp/databusdump/webids
-for i in `cat /tmp/databusdump/webids` ; do 
+for i in `cat /tmp/databusdump/webids` ; do
     rapper -i turtle -o ntriples $i >> /tmp/databusdump/webid.ttl
 done
 
-# pull the data ids
-curl -H "Accept: text/turtle" --data-urlencode "query@./queries/all-datasets.rq"  https://databus.dbpedia.org/repo/sparql > /tmp/databusdump/datasets.ttl
+
+echo "Pulling dataset metadata"
+QUERY_LIMIT=10000
+QUERY_OFFSET=0
+QUERY_BASE=$(<./queries/all-datasets.rq)
+
+QUERY="$QUERY_BASE LIMIT $QUERY_LIMIT OFFSET $QUERY_OFFSET"
+DUMP=$(curl -H "Accept: text/turtle" --data-urlencode "query=${QUERY}"  https://databus.dbpedia.org/repo/sparql)
+
+while [ "$DUMP" != "# Empty TURTLE" ]; do
+  echo "$DUMP" >> /tmp/databusdump/datasets.ttl
+  QUERY_OFFSET=$((QUERY_OFFSET + QUERY_LIMIT))
+  QUERY="$QUERY_BASE LIMIT $QUERY_LIMIT OFFSET $QUERY_OFFSET"
+  DUMP=$(curl -H "Accept: text/turtle" --data-urlencode "query=${QUERY}"  https://databus.dbpedia.org/repo/sparql)
+done
+
 
 chmod -R 777 /tmp/databusdump
 chmod -R 777 ${FOLDER}
@@ -31,24 +49,25 @@ rm $DATAPATH/download.lck
 # get the latest dataset
 LATEST=$(curl -H "Accept: text/plain" --data-urlencode "query@./queries/latest-dataset.rq"  https://databus.dbpedia.org/repo/sparql | sed 's/\s.*$//')
 echo "Latest loaded dataset is $LATEST. Updating query."
+echo "Waiting for new uploads..."
 
 QUERY=$(<./queries/new-datasets.rq)
 QUERY=${QUERY/DATASET/$LATEST}
 
-while true; do 
+while true; do
     sleep 60
     echo "Checking for new uploads..."
-   
-    NEW_DATASETS=$(curl -H "Accept: text/plain" --data-urlencode "query=${QUERY}"  https://databus.dbpedia.org/repo/sparql)
 
-    if [ "$NEW_DATASETS" != "# Empty NT" ]; then
-        FILE="$FOLDER/$(date +'%T%m%d').nt"
+    NEW_DATASETS=$(curl -H "Accept: text/turtle" --data-urlencode "query=${QUERY}"  https://databus.dbpedia.org/repo/sparql)
+
+    if [ "$NEW_DATASETS" != "# Empty TURTLE" ]; then
+        FILE="$FOLDER/$(date +'%T%m%d').ttl"
         echo ${NEW_DATASETS} > ${FILE}
         LATEST=$(curl -H "Accept: text/plain" --data-urlencode "query@./queries/latest-dataset.rq"  https://databus.dbpedia.org/repo/sparql | sed 's/\s.*$//')
         echo "New uploads dumped to $FILE"
         echo "Latest loaded dataset is $LATEST. Updating query."
+        echo "Waiting for new uploads..."
         QUERY=$(<./queries/new-datasets.rq)
         QUERY=${QUERY/DATASET/$LATEST}
     fi
 done
-
